4382833957e3843d15f272b86b4f7f3b4614dc3c
Sudhir Jaiswal <sudheerjaiswal1@gmail.com>
2023-05-09 19:03:32 +0530

    first commit

--- app.py
+++ app.py
@@ -4,32 +4,31 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        userInput = request.form["userInput"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(userInput),
+            temperature=1,
+            max_tokens=64,
         )
+        print(response)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def generate_prompt(userInput):
+    return """Provide context for userInput.
+userInput:provide a context to drinking tea
+context:Drinking tea is often associated with relaxation and comfort. Many people enjoy a cup of tea in the morning to start their day, or in the evening to wind down after a long day at work. Tea can be a great way to take a break from the hustle and bustle of daily life, and to savor a moment of peace and tranquility.
+userInput: {}
+context:""".format(
+        userInput.capitalize()
     )
